a0b1f963a06f4bc316932c735e54ba9b6ead8646
a <a@a.a>
2019-03-22 11:01:20 +0800

    init

---
+++ app/src/main/java/com/ubin/app/Main.java
@@ -0,0 +1,10 @@
+package com.ubin.app;
+
+import com.ubin.common.Utils;
+
+public class Main {
+
+    public static void main(String[] args){
+        Utils.Say("hello world");
+    }
+}
---
+++ common/src/main/java/com/ubin/common/Utils.java
@@ -0,0 +1,8 @@
+package com.ubin.common;
+
+public class Utils {
+
+    public static void Say(String something){
+        System.out.println(something);
+    }
+}
